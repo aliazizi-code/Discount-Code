bd2ecca2dabc28bc3f6af738a728dc0a5f39c89b
aliazizi-code <devaliazizi@gmail.com>
2024-11-10 17:42:22 +0330

    Complete products model: Add simple category and product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,48 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    title = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    url_title = models.SlugField(
+        max_length=65,
+        unique=True
+    )
+    parent = models.ForeignKey(
+        'ProductCategory',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    url_title = models.SlugField(
+        max_length=65,
+        null=False
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.title
+
